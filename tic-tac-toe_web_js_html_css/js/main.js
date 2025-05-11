35734a0492bd86cb7adf3ecfbdfbd19af202f413
ahmadharbi0x1337 <ahmadharbi0x1337@gmail.com>
2025-05-11 10:24:01 -0700

    Next is animateWinner()

--- tic-tac-toe_web_js_html_css/js/main.js
+++ tic-tac-toe_web_js_html_css/js/main.js
@@ -7,28 +7,58 @@ document.addEventListener("DOMContentLoaded", function(){
     let currentPlayer = "X";
     let gameOver = false;
     
-    // Create Cell Function , chekc CSS Coressponding styles for the class cell 
+    // Create Cell Function , check CSS Coressponding styles for the class cell, and pass the Click_event to handleCellClick 
     function createCell(){
         const cell = document.createElement("div");
         cell.classList.add("cell");
         cell.addEventListener("click", handleCellClick);
         return cell;
     }
+
     // Listen For Restart Button , Reload Page
     restartButton.addEventListener('click', function(){
         location.reload();
     });
+
     // Generate The Cells inside the game-board element
     for (let i = 0; i < 9; i++){
         board.appendChild(createCell());
     }
-    // Cell Click Event Function
+
+    // Click Cell Event Handler Function
     function handleCellClick(event){
         if (gameOver || event.target.textContent != "" || currentPlayer != "X"){
             return;
         }
         event.target.textContent = currentPlayer;
     }
+
+
+
+    // Check Winner By Comparing If Any Of The Winning Combinations is Acheived By The Same Char "X" OR "O"
+    function checkWinner(){
+ 	const cells = document.querySelectorAll(".cell");
+        const winCombinations = [
+            [0, 1, 2], [0, 3, 6], [3, 4, 5], [6, 7, 8], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
+        ];
+        for (combo of winCombinations){
+		const [a, b, c] = combo;
+		if (cells[a].textContent && cells[a].textContent === cells[b].textContent && cells[b].textContent === cells[b].textContent === cells[c]){
+			highlightWinner(cells[a], cells[b], cells[c]);
+			return true;
+		}
+	}
+	return false;
+    }
+
+    // Highlight Winner Function, Simply Adjust The CSS Styles in The Cells That Got Passed As Parameters
+    function highlightWinner(cellA, cellB, cellC){
+	    cellA.style.backgroundColor = ;
+	    cellB.style.backgroundColor = ;
+	    cellc.style.backgroundColor = ;
+    }
+
+    // Computer_Move Function.
     function computerMove(){
         const cells = document.querySelectorAll(".cell");
         let availableCells = [];
@@ -45,6 +75,9 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     }
 
+
+    // Finding Best Move Through Winning Combinations, and if not possible block the X winning combinations
+    // if either of the conditions cannot be satisfied, then put a random O, and a strategic first move to put O in the center if not occupied.
     function findBestMove(){
         const cells = document.querySelectorAll(".cell");
         const winCombinations = [
